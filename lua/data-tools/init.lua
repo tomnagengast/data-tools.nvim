126c4c713fa79d556b2c37b6a42dad3166ff6008
tnagengast <tnagengast@gmail.com>
2023-07-19 17:20:37 -0700

    Update selection to include ref statements

--- lua/data-tools/init.lua
+++ lua/data-tools/init.lua
@@ -1,25 +1,34 @@
+local ts_utils = require('nvim-treesitter.ts_utils')
 local M = {}
-
--- vim.api.nvim_notify("Loaded data-tools", vim.log.levels.INFO, {})
--- vim.api.nvim_notify("This is a warning message", vim.log.levels.WARN, {})
 
 M.setup = function(opts)
     print("Options: ", opts)
 end
 
-M.current_path = function()
-    return vim.fn.expand("%:p")
+local get_select_statment_range = function()
+    local node = ts_utils.get_node_at_cursor()
+    if node == nil then
+        error("No Treesitter parser found")
+    end
+
+    while node ~= nil and node:type() ~= 'statement' do
+        node = node:parent()
+    end
+    local start_row, start_col, end_row, end_col = node:range()
+
+    if node:next_sibling() ~= nil and node:next_sibling():type() == 'ERROR' then
+        _, _, end_row, end_col = node:next_sibling():range()
+    end
+
+    return {start_row, start_col, end_row, end_col}
 end
 
-print('test ' .. M.current_path())
+M.select = function()
+    local range = get_select_statment_range()
+    local bufnr = vim.api.nvim_get_current_buf()
+    ts_utils.update_selection(bufnr, range)
+end
+
+vim.cmd('command! DTSelectStatement echo luaeval("require(\'data-tools\').select()")')
 
 return M
-
---[[
-
-lua P(require('data-tools'))
-lua P(require('data-tools').example())
-lua P(require('data-tools').setup({opt1 = 'value1', opt2 = 'value2'}))
-lua R('data-tools')
-
---]]
